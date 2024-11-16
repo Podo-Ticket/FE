60df4384fcfe3f5c86c49d2df2e189f4fc28e4df
YBSeok <soberyl4304@gmail.com>
2024-11-16 16:10:37 +0900

    fix: https 수정

--- src/constants/ServerURL.js
+++ src/constants/ServerURL.js
@@ -1,4 +1,4 @@
 // export const SERVER_URL = "http://localhost:8080";
-// export const SERVER_URL = "https://api.podo-ticket.com";
-export const SERVER_URL = "http://43.203.226.86:8080";
+export const SERVER_URL = "https://api.podo-ticket.com";
+// export const SERVER_URL = "http://43.203.226.86:8080";
 
