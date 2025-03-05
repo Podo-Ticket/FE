61b90f51bc0a371bfdb0ff13a6fc488a9f503229
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-03-05 18:52:34 +0900

    fix: update font style for in-app install button

--- src/components/button/ModalSmallBtn.tsx
+++ src/components/button/ModalSmallBtn.tsx
@@ -1,49 +1,69 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 interface SmallBtnProps {
-    content: string;        // 버튼 안 내용
-    onClick: () => void;        // 온 클릭 함수
-    isAvailable: boolean;       // 버튼 동작 여부
-    isDarkblue?: boolean;        // 남색 버튼 여부
+  content: string; // 버튼 안 내용
+  onClick: () => void; // 온 클릭 함수
+  isAvailable: boolean; // 버튼 동작 여부
+  isDarkblue?: boolean; // 남색 버튼 여부
 }
 
-const ModalSmallBtn: React.FC<SmallBtnProps> = ({ content, onClick, isAvailable, isDarkblue = false }) => {
-    return (
-        <ModalSmallBtnContainer
-            className='text-lg-300'
-            onClick={onClick}
-            disabled={!isAvailable}
-            isAvailable={isAvailable}
-            isDarkblue={isDarkblue}
-        >{content}</ModalSmallBtnContainer>
-    )
-}
+const ModalSmallBtn: React.FC<SmallBtnProps> = ({
+  content,
+  onClick,
+  isAvailable,
+  isDarkblue = false,
+}) => {
+  return (
+    <ModalSmallBtnContainer
+      className="Podo-Ticket-Body-B4"
+      onClick={onClick}
+      disabled={!isAvailable}
+      isAvailable={isAvailable}
+      isDarkblue={isDarkblue}
+    >
+      {content}
+    </ModalSmallBtnContainer>
+  );
+};
 
-export default ModalSmallBtn
+export default ModalSmallBtn;
 
-const ModalSmallBtnContainer = styled.button<{ isAvailable: boolean, isDarkblue: boolean }>`
-display: flex;
-justify-content: center;
-align-items: center;
+const ModalSmallBtnContainer = styled.button<{
+  isAvailable: boolean;
+  isDarkblue: boolean;
+}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-width: 8.4375rem;
-height: 3.125rem;
+  width: 9.375rem;
+  height: 2.875rem;
 
-padding: 11px 48px;
-border-radius: 15px;
-background: ${({ isAvailable, isDarkblue }) => isDarkblue ? 'var(--grey-3)' : (isAvailable ? 'var(--purple-4)' : 'var(--purple-9)')};
-border: none;
+  padding: 11px 48px;
+  border-radius: 15px;
+  background: ${({ isAvailable, isDarkblue }) =>
+    isDarkblue
+      ? "var(--grey-3)"
+      : isAvailable
+      ? "var(--purple-4)"
+      : "var(--purple-9)"};
+  border: none;
 
-gap: 10px;
+  gap: 10px;
 
-color: ${({ isAvailable, isDarkblue }) => isDarkblue ? 'var(--grey-6)' : (isAvailable ? 'var(--ect-white)' : 'var(--ect-white)')};
-text-align: center;
+  color: ${({ isAvailable, isDarkblue }) =>
+    isDarkblue
+      ? "var(--grey-6)"
+      : isAvailable
+      ? "var(--ect-white)"
+      : "var(--ect-white)"};
+  text-align: center;
 
-transition: background 0.3s ease-in-out;
+  transition: background 0.3s ease-in-out;
 
-user-select: none; /* 텍스트 선택 방지 */
--webkit-user-select: none; /* Safari에서 드래그 방지 */
--moz-user-select: none; /* Firefox에서 드래그 방지 */
--ms-user-select: none;
+  user-select: none; /* 텍스트 선택 방지 */
+  -webkit-user-select: none; /* Safari에서 드래그 방지 */
+  -moz-user-select: none; /* Firefox에서 드래그 방지 */
+  -ms-user-select: none;
 `;
