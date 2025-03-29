0f9911a877095562c05b341b43d0b1e72e5bb2e5
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-03-30 03:50:10 +0900

    💄style: Delete border

--- src/components/common/buttons/WhiteBoxBtn.tsx
+++ src/components/common/buttons/WhiteBoxBtn.tsx
@@ -63,7 +63,6 @@ const ButtonTitle = styled.div`
   display: flex;
   align-items: center;
   width: 100%;
-  border: 1px solid var(--red-2);
 `;
 const LeftContent = styled.div`
   width: 100%;
@@ -75,8 +74,6 @@ const LeftContent = styled.div`
   gap: ${pxToVw(6)};
   flex-glow: 1;
   text-align: left;
-
-  border: 1px solid var(--red-2);
 `;
 
 const Icon = styled.img`
@@ -108,7 +105,6 @@ const LeftIcon = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-
 
   @media (max-resolution: 2dppx) {
     width: 30px;
