5367c3329ec853d6ad327b26dcf70220825f56a4
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-04-06 23:19:20 +0900

    ✨feat: show modal when admin session is expired (#48)

--- src/utils/ProtectedRoute.tsx
+++ src/utils/ProtectedRoute.tsx
@@ -12,15 +12,20 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true); // 세션 확인 중 로딩 상태
   const [isSessionValid, setIsSessionValid] = useState(false); // 세션 유효성 상태
   const [showNoticeModal, setShowNoticeModal] = useState(false);
+  const [redirect, setRedirect] = useState(false); // 확인 후 리다이렉트 여부
 
   useEffect(() => {
     const checkSession = async () => {
       try {
         const response = await verifyAdminSession(); // 세션 확인 API 호출
         setIsSessionValid(response.session); // 세션 유효성 설정
+        if (!response.session) {
+          setShowNoticeModal(true); // 세션 만료 시 모달 띄움
+        }
       } catch (error) {
         console.error("세션 확인 중 오류 발생:", error);
         setIsSessionValid(false); // 세션이 유효하지 않음으로 설정
+        setShowNoticeModal(true);
       } finally {
         setIsLoading(false); // 로딩 완료
       }
@@ -29,25 +34,32 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
     checkSession();
   }, []);
 
-  if (isLoading) {
-    return <div></div>; // 로딩 상태 표시
+  if (isLoading) return <div />;
+
+  if (redirect) {
+    return <Navigate to="/adminAuth" replace />;
   }
 
   if (!isSessionValid) {
-    return <Navigate to="/adminAuth" replace />; // 세션이 만료된 경우 리디렉션
+    return (
+      <>
+        {showNoticeModal && (
+          <NoticeModal
+            showNoticeModal={true}
+            title="인증코드를 다시 입력해주세요!"
+            description="세션이 만료되었습니다."
+            onAcceptFunc={() => {
+              setShowNoticeModal(false);
+              setRedirect(true); // 모달 닫은 뒤 리다이렉트
+            }}
+            buttonContent="확인"
+          />
+        )}
+      </>
+    );
   }
 
-  return <>
-    {showNoticeModal && (
-      <NoticeModal
-        showNoticeModal={true}
-        title="인증코드를 다시 입력해주세요!"
-        description="세션이 만료되었습니다."
-        onAcceptFunc={() => setShowNoticeModal(false)}
-        buttonContent="확인"
-      />
-    )}
-    {isSessionValid && children}</>; // 세션이 유효한 경우 자식 컴포넌트 렌더링
+  return <>{children}</>;
 };
 
 export default ProtectedRoute;
