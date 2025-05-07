6d7485b8216c2058047ed189a6b24205ed1db69b
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-05-07 16:30:11 +0900

    chore: fix formatting in privacy policy

--- src/constants/text/UIText.ts
+++ src/constants/text/UIText.ts
@@ -1,78 +1,96 @@
 export const PERSONAL_INFORMATION_AGREE_CONTENT = {
   korean: {
     title: `개인정보 수집 약관동의 (필수)`,
-    detail: `
-개인정보보호법에 따라 <포도티켓>에 회원가입 하시는 분께 수집하는 개인정보의 항목, 개인정보의 수집 및 이용목적, 개인정보의 보유 및 이용기간, 동의 거부권 및 동의 거부 시 불이익에 관한 사항을 안내해 드리오니 자세히 읽은 후 동의하여 주시기를 바랍니다.
+    detail: ` 개인정보보호법에 따라 <포도티켓>에 회원가입 하시는 분께 수집하는 개인정보의 항목, 개인정보의 수집 및 이용목적, 개인정보의 보유 및 이용기간, 동의 거부권 및 동의 거부 시 불이익에 관한 사항을 안내해 드리오니 자세히 읽은 후 동의하여 주시기를 바랍니다.
+
 ■ 수집하는 개인정보 항목
+
 <포도티켓>은 회원 서비스 운영, 판매 상품 A/S, 주문 및 결제, 서비스 홍보 및 판매 권유를 위해 아래와 같은 개인정보를 수집하고 있습니다.
-수집항목: 이름, 전화번호
-개인정보 수집 방법: 홈페이지(회원가입), 공연 예매
+
+    - 수집항목: 이름, 전화번호
+    - 개인정보 수집 방법: 홈페이지(회원가입), 공연 예매
+
+
 ■ 개인정보의 수집 및 이용 목적
-포도티켓은 수집한 개인정보를 다음의 목적을 위해 활용합니다.
-재화 또는 서비스 제공
-서비스 제공, 계약서 ∙ 청구서 발송, 콘텐츠 제공, 맞춤 서비스 제공, 본인인증, 연령 인증, 요금 결제 ∙ 정산
-회원 가입 및 관리
-회원 가입 의사 확인, 회원제 서비스 제공에 따른 본인 식별 ∙ 인증, 회원 자격 유지 ∙ 관리, 서비스 부정 이용 방지, 만 14세 미만 아동의 개인정보 처리 시 법정대리인의 동의 여부 확인, 각종 고지 ∙ 통지, 고충 처리
+<포도티켓>은 수집한 개인정보를 다음의 목적을 위해 활용합니다.
+
+    - 재화 또는 서비스 제공
+    - 서비스 제공, 계약서 ∙ 청구서 발송, 콘텐츠 제공, 맞춤 서비스 제공, 본인인증, 연령 인증, 요금 결제 ∙ 정산
+    - 회원 가입 및 관리
+    - 회원 가입 의사 확인, 회원제 서비스 제공에 따른 본인 식별 ∙ 인증, 회원 자격 유지 ∙ 관리, 서비스 부정 이용 방지, 만 14세 미만 아동의 개인정보 처리 시 법정대리인의 동의 여부 확인, 각종 고지 ∙ 통지, 고충 처리
+
+
 ■ 개인정보의 보유 및 이용기간
-포도티켓은 개인정보 수집 및 이용 목적이 달성된 후에는 예외 없이 해당 정보를 지체 없이 파기합니다.
+
+<포도티켓>은 개인정보 수집 및 이용 목적이 달성된 후에는 예외 없이 해당 정보를 지체 없이 파기합니다.
+
 자세한 내용은 개인정보 처리 방침을 확인해 주세요.
+
 귀하는 위와 같이 개인정보를 수집 ∙ 이용하는데 동의를 거부할 권리가 있습니다. 필수 수집 항목에 대해 동의를 거절하는 경우 서비스 이용이 제한될 수 있습니다.
+
+
 ■ 개인정보 제3자에게 제공 동의
-포도티켓은 수집·보유하고 있는 개인정보를 이용자의 동의 없이 제3자에게 제공하지 않으나, 다음의 경우에는 개인정보를 제3자에게 제공할 수 있습니다.
-포도티켓 회원 가입을 위하여 아래의 개인정보 수집 이용(및 제공)에 대한 내용을 자세히 읽어 보신 후 동의 여부를 결정하여 주시기 바랍니다.
-공연 예매, 좌석 배정 및 입장을 위하여 공연 주체에게 전달하는 경우
-법률에 특별한 규정이 있거나 법령상 의무를 준수하기 위하여 불가피한 경우
-제공받는 자: 해당 공연 주체
-제공목적: 좌석 배정, 공연 입장
-제공하는 항목: 이름, 전화번호
-보유 및 이용기간: 해당 계약 종료 시점 이전까지
-귀하는 위와 같이 개인정보를 수집 ∙ 이용하는데 동의를 거부할 권리가 있습니다. 필수 수집 항목에 대해 동의를 거절하는 경우 서비스 이용이 제한될 수 있습니다.`,
+<포도티켓>은 수집·보유하고 있는 개인정보를 이용자의 동의 없이 제3자에게 제공하지 않으나, 다음의 경우에는 개인정보를 제3자에게 제공할 수 있습니다.
+
+<포도티켓> 회원 가입을 위하여 아래의 개인정보 수집 이용(및 제공)에 대한 내용을 자세히 읽어 보신 후 동의 여부를 결정하여 주시기 바랍니다.
+
+
+    - 공연 예매, 좌석 배정 및 입장을 위하여 공연 주체에게 전달하는 경우
+    - 법률에 특별한 규정이 있거나 법령상 의무를 준수하기 위하여 불가피한 경우
+
+    제공받는 자: 해당 공연 주체
+    제공목적: 좌석 배정, 공연 입장
+    제공하는 항목: 이름, 전화번호
+    보유 및 이용기간: 해당 계약 종료 시점 이전까지
+
+
+  귀하는 위와 같이 개인정보를 수집 ∙ 이용하는데 동의를 거부할 권리가 있습니다. 필수 수집 항목에 대해 동의를 거절하는 경우 서비스 이용이 제한될 수 있습니다.`,
   },
   english: {
     title: `Consent to Personal Data Collectio`,
-    detail: `
-In accordance with the Personal Information Protection Act, <PodoTicket> informs members registering on our platform about the items of personal information collected, the purposes of collection and use, the retention and usage period of the personal information, the right to refuse consent, and any disadvantages that may arise from such refusal. Please read the following carefully before giving your consent.
+    detail: ` In accordance with the Personal Information Protection Act, <PodoTicket> informs members registering on our platform about the items of personal information collected, the purposes of collection and use, the retention and usage period of the personal information, the right to refuse consent, and any disadvantages that may arise from such refusal. Please read the following carefully before giving your consent.
+
 
 ■ Items of Personal Information Collected
 <PodoTicket> collects the following personal information for the operation of member services, after-sales service for sold products, order and payment processing, and for service promotion and marketing purposes.
 
-Items collected: Name, Phone number
+    - Items collected: Name, Phone number
+    - Collection methods: Website (membership registration), performance ticket reservation
 
-Collection methods: Website (membership registration), performance ticket reservation
 
 ■ Purpose of Collection and Use of Personal Information
-PodoTicket uses the collected personal information for the following purposes:
+<PodoTicket> uses the collected personal information for the following purposes:
 
-Provision of goods or services
+    - Provision of goods or services
 
-Service provision, sending contracts and invoices, providing content, personalized services, identity verification, age verification, payment and settlement
+    - Service provision, sending contracts and invoices, providing content, personalized services, identity verification, age verification, payment and settlement
 
-Membership registration and management
+    - Membership registration and management
 
-Confirming intent to register, personal identification and authentication for membership services, maintaining and managing membership status, preventing unauthorized use of services, verifying legal guardian consent for processing personal information of children under 14, notifications and announcements, handling complaints
+    - Confirming intent to register, personal identification and authentication for membership services, maintaining and managing membership status, preventing unauthorized use of services, verifying legal guardian consent for processing personal information of children under 14, notifications and announcements, handling complaints
+
 
 ■ Retention and Use Period of Personal Information
-PodoTicket will destroy the collected personal information without delay once the purpose of collection and use has been achieved, without exception.
+<PodoTicket> will destroy the collected personal information without delay once the purpose of collection and use has been achieved, without exception.
+
 For more details, please refer to our Privacy Policy.
+
 You have the right to refuse consent to the collection and use of personal information as described above. However, refusal to consent to required items may limit your ability to use the service.
 
+
 ■ Consent to Provision of Personal Information to Third Parties
-PodoTicket does not provide collected and held personal information to third parties without the user's consent, except in the following cases:
-Please read the details below regarding the collection, use (and provision) of personal information for PodoTicket membership registration, and decide whether to give your consent.
+<PodoTicket> does not provide collected and held personal information to third parties without the user's consent, except in the following cases:
+Please read the details below regarding the collection, use (and provision) of personal information for <PodoTicket> membership registration, and decide whether to give your consent.
 
-When information is provided to the event organizer for ticket reservation, seat assignment, and admission
+    - When information is provided to the event organizer for ticket reservation, seat assignment, and admission
+    - When required by law or to comply with legal obligations
 
-When required by law or to comply with legal obligations
+    Recipient: The respective event organizer
+    Purpose of provision: Seat assignment, event admission
+    Items provided: Name, Phone number
+    Retention and use period: Until the end of the relevant contract
 
-Recipient: The respective event organizer
-
-Purpose of provision: Seat assignment, event admission
-
-Items provided: Name, Phone number
-
-Retention and use period: Until the end of the relevant contract
-
-You have the right to refuse consent to the collection and use of personal information as described above. However, refusal to consent to required items may limit your ability to use the service.`,
+  You have the right to refuse consent to the collection and use of personal information as described above. However, refusal to consent to required items may limit your ability to use the service.`,
   },
 };
 
